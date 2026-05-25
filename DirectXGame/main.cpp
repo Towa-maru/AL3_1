18d61ba491bde87628fbbcb2c69b1e4016fac760
Towa-maru <k025g1040@g.neec.ac.jp>
2026-05-25 14:25:47 +0900

    初期化

--- DirectXGame/main.cpp
+++ DirectXGame/main.cpp
@@ -1,7 +1,38 @@
 #include <Windows.h>
+#include"KamataEngine.h"
+#include"GaneScene.h"
+
+using namespace KamataEngine;
 
 // Windowsアプリでのエントリーポイント(main関数)
 int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int) {
-	int a = 10;
+	
+	DirectXCommon* dxCommon = DirectXCommon::GetInstance();
+
+	GameScene* gameScene = new GameScene();
+	gameScene->Initialize();
+
+	KamataEngine::Initialize(L"GC1C_05_タシロ_トワ");
+
+	while(true) {
+		if (KamataEngine::Update()) {
+			break;
+		}
+
+		gameScene->Update();
+
+		dxCommon->PreDraw();
+
+		gameScene->Draw();
+
+		dxCommon->PostDraw();
+
+	}
+
+	KamataEngine::Finalize();
+
+	delete gameScene;
+	gameScene = nullptr;
+
 	return 0;
 }
